6ff00fa178b5b6103151c7588df1105661ce0377
QianChengLong <qian_cheng_long@163.com>
2015-01-23 11:14:42 +0800

    修改

--- cmake/internal_utils.cmake
+++ cmake/internal_utils.cmake
@@ -21,17 +21,15 @@ function(cxx_library name)
       set(type STATIC)
   endif()
   add_library(${name} ${type} ${ARGN})
-  message(STATUS "${name} ${type} ${ARGN}")
 endfunction()
 
 # 使用第三方库，添加include路径，lib路径
-function(use_library)
+function(use_libraries)
     foreach(name ${ARGN})
         if(${name} STREQUAL ".")
             include_directories(${PROJECT_SOURCE_DIR}/include)
             link_directories(${PROJECT_SOURCE_DIR}/lib)
         else()
-            message(${PROJECT_SOURCE_DIR}/3rdparty/${name}/include)
             include_directories(${PROJECT_SOURCE_DIR}/3rdparty/${name}/include)
             link_directories(${PROJECT_SOURCE_DIR}/3rdparty/${name}/lib)
         endif()
@@ -39,16 +37,22 @@ function(use_library)
 endfunction()
 
 # 单元测试需要链接的库
-function(utest_link)
+function(utest_link_libraries)
     set(UTEST_LIBRARIES ${ARGN} CACHE INTERNAL "UTEST_LIBRARIES")
 endfunction()
 
 # 添加单元测试
 # @param [in] name 单元测试名，被测试文件名
 function(add_utest name)
-    # message("${UTEST_LIBRARIES}")
     set(TESTCASE ${name}_test)
     add_executable(${TESTCASE} ${name}_test.cpp)
     target_link_libraries(${TESTCASE} ${UTEST_LIBRARIES})
     add_test(${PROJECT_NAME} ${TESTCASE})
+endfunction()
+
+# 忽略指定警告
+function(ignore_warnings)
+    foreach(num ${ARGN})
+        add_definitions(/wd"${num}")
+    endforeach()
 endfunction()
